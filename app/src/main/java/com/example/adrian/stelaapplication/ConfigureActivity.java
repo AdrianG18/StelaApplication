9ed163d8a188e1b422b4b18b444f94d1074e82a4
pandrews <pandrews@g.hmc.edu>
2018-02-19 19:19:12 -0800

    added Configure activity

---
+++ app/src/main/java/com/example/adrian/stelaapplication/ConfigureActivity.java
@@ -0,0 +1,18 @@
+package com.example.adrian.stelaapplication;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by parkerandrews on 2/15/18.
+ */
+
+public class ConfigureActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
