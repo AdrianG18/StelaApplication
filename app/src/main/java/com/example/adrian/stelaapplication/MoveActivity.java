06bbeb49580b712065f93b7c2ca57ae4a31cc28f
pandrews <pandrews@g.hmc.edu>
2018-02-28 20:16:11 -0800

    updated MoveActivity

--- app/src/main/java/com/example/adrian/stelaapplication/MoveActivity.java
+++ app/src/main/java/com/example/adrian/stelaapplication/MoveActivity.java
@@ -3,6 +3,7 @@ package com.example.adrian.stelaapplication;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -35,11 +36,63 @@ public class MoveActivity extends AppCompatActivity {
 //         client = new StelaClient(getApplicationContext());
     }
 
+//    public boolean verifyMove(View v) {
+//        boolean verify;
+//        String s1 = xText.getText().toString();
+//        String s2 = yText.getText().toString();
+//        if ()
+//
+//    }
+
+//  public void errorPopup() {
+//        AlertDialog.Builder builder1 = new AlertDialog.Builder(context);
+//        builder1.setMessage("Write your message here.");
+//        builder1.setCancelable(true);
+//
+//        builder1.setPositiveButton(
+//                "Yes",
+//                new DialogInterface.OnClickListener() {
+//                    public void onClick(DialogInterface dialog, int id) {
+//                        dialog.cancel();
+//                    }
+//                });
+//
+//        builder1.setNegativeButton(
+//                "No",
+//                new DialogInterface.OnClickListener() {
+//                    public void onClick(DialogInterface dialog, int id) {
+//                        dialog.cancel();
+//                    }
+//                });
+//
+//        AlertDialog alert11 = builder1.create();
+//        alert11.show();
+//
+//
+//    }
+
+
     public void onMove(View v) {
         // get the coordinates from the number values they input
-        Double x = Double.parseDouble(xText.getText().toString());
-        Double y = Double.parseDouble(yText.getText().toString());
+        String s1 = xText.getText().toString();
+        String s2 = yText.getText().toString();
+
+        if (s1.isEmpty()) {
+            // print some error message popup thing
+            Log.d("Null Pointer", "X-Coordinate must have a value");
+            return;
+        }
+        if (s2.isEmpty()) {
+            // print some error message popup thing
+            Log.d("Null Pointer", "Y-Coordinate must have a value");
+            return;
+        }
+
+        Double x = Double.parseDouble(s1);
+        Double y = Double.parseDouble(s2);
+
         if (!(x instanceof Double) || !(y instanceof Double)) {
+            Log.d("Type Error", "Not a Double");
             return;
         }
 
